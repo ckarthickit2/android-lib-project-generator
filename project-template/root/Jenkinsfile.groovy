9840f716b1468c85ac4e7cb6bfe0639e29d7941e
CHINNATHAMBI, KARTHICK (kc3503) <karthick.chinnathambi@intl.att.com>
2019-08-09 16:44:07 +0530

    Update/Format Jenkinsfile to suit multi-branch pipeline

--- project-template/root/Jenkinsfile.groovy
+++ project-template/root/Jenkinsfile.groovy
@@ -1,13 +1,13 @@
 pipeline {
 
   agent {
-          label "ios"
+    label "ios"
   }
 
   //agent any
 
   environment {
-    IS_RELEASE = "${(branch == 'master')}"
+    IS_RELEASE = "${(BRANCH_NAME == 'master')}"
   }
 
   options {
@@ -19,48 +19,50 @@ pipeline {
 
     stage('Git checkout') {
       steps {
-          sh "echo '##Chosen Branch : ${branch}##'"
-          checkout([$class                           : 'GitSCM',
-                    branches                         : [[name: "${branch}"]],
-                    doGenerateSubmoduleConfigurations: false,
-                    extensions                       : [[$class: 'CleanCheckout']],
-                    gitTool                          : 'Default',
-                    submoduleCfg                     : [],
-                    userRemoteConfigs                : [[credentialsId: 'svc-qpjenkins-egbbk',
-                                                         name         : 'origin',
-                                                         url          : 'ssh://git@egbitbucket.dtvops
-                                                         .net:7999/templateproj/templaterepo.git']]
-          ])
+        echo "##Building Branch : $BRANCH_NAME##"
+        checkout([$class                           : 'GitSCM',
+                  branches                         : [[name: "$BRANCH_NAME"]],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions                       : [[$class: 'CleanCheckout']],
+                  gitTool                          : 'Default',
+                  submoduleCfg                     : [],
+                  userRemoteConfigs                : [[credentialsId: 'svc-qpjenkins-egbbk',
+                                                       name         : 'origin',
+                                                       url          : 'ssh://git@domain:port/repo_path.git']]
+        ])
       }
     }
 
     //Build Libraries and Publish to Maven Local
     stage('Build Libraries') {
       steps {
-          sh "./gradlew publishLibrariesToMavenLocal -PisRelease=$IS_RELEASE"
+        //sh  "yes | $ANDROID_HOME/tools/bin/sdkmanager build-tools;29.0.1"
+        sh "git submodule update --recursive"
+        sh "./gradlew clean && ./gradlew assemble -PisRelease=$IS_RELEASE"
       }
     }
 
     //Publish the Build artifacts to specified Maven Repository
     stage('Publish') {
       environment {
-          NEXUS_CREDS = credentials('svc-jenkins-nexus-cicd')
+        NEXUS_CREDS = credentials('svc-jenkins-nexus-cicd')
       }
       steps {
-          sh "./gradlew publish -PisRelease=$IS_RELEASE -PnexusUsername=$NEXUS_CREDS_USR -PnexusPassword=$NEXUS_CREDS_PSW"
+        sh "uname -a"
+        sh "./gradlew publish -PisRelease=$IS_RELEASE -PnexusUsername=$NEXUS_CREDS_USR -PnexusPassword=$NEXUS_CREDS_PSW"
       }
     }
 
     //Bump Up the version and commit a Tag to Git
     stage('Bump Version') {
       when {
-          //branch 'master' //Works only for multibranch pipeline
-          environment name: 'IS_RELEASE', value: 'true'
+        //branch 'master' //Works only for multibranch pipeline
+        environment name: 'IS_RELEASE', value: 'true'
       }
       steps {
-          sshagent(['svc-qpjenkins-egbbk']) {
-              sh "./gradlew bumpVersionAndTag -PisRelease=$IS_RELEASE"
-          }
+        sshagent(['svc-qpjenkins-egbbk']) {
+          sh "./gradlew bumpVersionAndTag -PisRelease=$IS_RELEASE"
+        }
       }
     }
 
